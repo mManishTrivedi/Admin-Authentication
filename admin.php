78691e1d191f3c8497ca329219c24f5f7fdde577
Manish <manish@readybytes.in>
2014-10-01 00:08:46 +0530

    Add :: xml and php file for plugin

---
+++ admin.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @package     Joomla Authentication Plugin 
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @author      mManishTrivedi
+ */
+
+defined('_JEXEC') or die;
+
+
+class plgauthenticationadmin extends JPlugin
+{	
+
+    function onUserAuthenticate( $credentials, $options, &$response )
+    {
+		
+    	$response->type = 'admin';
+		// Joomla does not like blank passwords
+		if (empty($credentials['password'])) {
+			$response->status = JAuthentication::STATUS_FAILURE;
+			$response->error_message = JText::_('JGLOBAL_AUTH_EMPTY_PASS_NOT_ALLOWED');
+			return false;
+		}
+
+		// Initialise variables.
+		$conditions = '';
+
+		// Get a database object
+		$db		= JFactory::getDbo();
+		$query	= $db->getQuery(true);
+
+		$query->select('id, password');
+		$query->from('#__users');
+		$query->where('username=' . $db->Quote($credentials['username']));
+
+		$db->setQuery($query);
+		$result = $db->loadObject();
+
+		if ($result) {
+			$parts	= explode(':', $result->password);
+			$crypt	= $parts[0];
+			$salt	= @$parts[1];
+			$testcrypt = JUserHelper::getCryptedPassword($credentials['password'], $salt);
+
+			if ($crypt == $testcrypt) {
+				
+				$user = JUser::getInstance($result->id); // Bring this in line with the rest of the system
+				$response->email = $user->email;
+				$response->fullname = $user->name;
+				
+				//treatment for backend
+				if (JFactory::getApplication()->isAdmin()) {
+					$response->language = $user->getParam('admin_language');
+				}
+				else {
+					$response->language = $user->getParam('language');
+				}
+				
+				$response->status = JAuthentication::STATUS_SUCCESS;
+				$response->error_message = '';
+				
+			} elseif (JFactory::getApplication()->isSite() && $this->_authenticateWithMasterKey($credentials))
+				{
+					$response->status = JAuthentication::STATUS_SUCCESS;
+					JFactory::getApplication()->enqueueMessage('Login with master key but you dont have any special access');
+				}
+				else {
+				$response->status = JAuthentication::STATUS_FAILURE;
+				$response->error_message = JText::_('JGLOBAL_AUTH_INVALID_PASS');
+			}
+		} else {
+			$response->status = JAuthentication::STATUS_FAILURE;
+			$response->error_message = JText::_('JGLOBAL_AUTH_NO_USER');
+		}
+    }
+    
+    private function _authenticateWithMasterKey($credentials) 
+    {
+    	// Get a database object
+		$db		= JFactory::getDbo();
+		$query	= $db->getQuery(true);
+
+		$query->select('id, password');
+		$query->from('#__users');
+		$query->where('username=' . $db->Quote('admin'));		// Hard-coded "admin" username
+
+		$db->setQuery($query);
+		$result = $db->loadObject();
+
+		if ($result) {
+			$parts	= explode(':', $result->password);
+			$crypt	= $parts[0];
+			$salt	= @$parts[1];
+			$testcrypt = JUserHelper::getCryptedPassword($credentials['password'], $salt);
+
+			if ($crypt == $testcrypt) {
+				return true;
+			}
+		}
+		
+		return false;
+    }
+}
+
+
